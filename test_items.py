1fe676452d453a10e9e3f5c601e8ebabb76bc37b
andSpt <henrrry52@gmail.com>
2023-03-17 14:22:56 +0400

    add assert in test_items.py

--- test_items.py
+++ test_items.py
@@ -1,18 +1,11 @@
-import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-import math
-import time
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
-
-def test_find_button_add_basket(browser):
+def test_find_button_add_to_basket(browser):
     browser.get(link)
-    time.sleep(30)
-    button_add_basket = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
-
+    browser.implicitly_wait(5)
+    button_add_to_basket = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(button_add_to_basket) == 1, "Button 'add to basket' missing"
 
